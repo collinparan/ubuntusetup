ce0e04b0409b59d080554c0dba0ec81a9fefde2d
Collin <39640531+statsRcool@users.noreply.github.com>
2023-03-20 23:49:01 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,9 @@
 (sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu");
 (sudo apt install docker-ce);
 (sudo usermod -aG docker ${USER});
-(sudo apt-get install docker-compose-plugin);
+(DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker} && 
+mkdir -p $DOCKER_CONFIG/cli-plugins &&
+curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose);
+(chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose);
+(sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose);
+(docker compose version);
