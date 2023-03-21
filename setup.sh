a5972ae636327a6ad1caf912d140b996967330e6
Collin <39640531+statsRcool@users.noreply.github.com>
2023-03-21 06:14:46 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,33 @@
-(sudo apt update);
-(sudo apt install apt-transport-https ca-certificates curl software-properties-common);
-(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -);
-(sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable");
-(sudo apt install docker-ce);
-(sudo usermod -aG docker ${USER});
-(DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker} && 
-mkdir -p $DOCKER_CONFIG/cli-plugins &&
-curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose);
-(chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose);
-(sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose);
-(docker compose version);
+#!/bin/bash
+
+# Update package index and install required dependencies
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update package index and install Docker CE
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# Add current user to the docker group to avoid using sudo when running docker commands
+sudo usermod -aG docker $(whoami)
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the Docker Compose binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Print installed versions
+echo "Docker version:"
+docker --version
+echo "Docker Compose version:"
+docker-compose --version
+
+echo "Docker and Docker Compose installation completed successfully."
+
