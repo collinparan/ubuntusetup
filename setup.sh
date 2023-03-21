dbb246a55cf3f36322f2d863b53dcf6e7732d8a9
Collin <39640531+statsRcool@users.noreply.github.com>
2023-03-20 23:31:17 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,4 @@
 (sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable");
 (sudo apt install docker-ce);
 (sudo usermod -aG docker ${USER});
-(sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose);
-(sudo chmod +x /usr/local/bin/docker-compose);
-(docker-compose --version);
+(sudo apt-get install docker-compose-plugin);
