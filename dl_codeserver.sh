c87b461c8fde30ba1a9a3afe8141a4a46b4d4ea3
Collin <39640531+statsRcool@users.noreply.github.com>
2023-06-23 18:54:29 -0600

    Update dl_codeserver.sh

--- dl_codeserver.sh
+++ dl_codeserver.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-(wget https://github.com/coder/code-server/releases/download/v4.11.0/code-server_4.11.0_amd64.deb);
+(wget https://github.com/coder/code-server/releases/download/v4.14.0/code-server_4.14.0_amd64.deb);
 
 (sudo apt install ./code-server_*_amd64.deb);
 
