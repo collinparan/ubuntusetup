8bd343a93bc07d959872c20511f06a870d6a3854
Collin <39640531+collinparan@users.noreply.github.com>
2023-12-30 16:53:09 -0700

    Update agx_xavier_setup.sh

--- agx_xavier_setup.sh
+++ agx_xavier_setup.sh
@@ -2,6 +2,8 @@
 sudo apt-get install apt-utils;
 sudo apt-get install curl;
 
+dpkg -l | grep 'nvidia-l4t-core';
+
 sudo apt install cuda-toolkit-11-4;
 
 export PATH=/usr/local/cuda-11.4/bin:$PATH;
