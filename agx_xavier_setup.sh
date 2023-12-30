ec6a52bf9c0e2cf81b0243dd1c72f6077ea48635
Collin <39640531+collinparan@users.noreply.github.com>
2023-12-30 16:40:04 -0700

    Update agx_xavier_setup.sh

--- agx_xavier_setup.sh
+++ agx_xavier_setup.sh
@@ -1,3 +1,10 @@
 #install CUDA
 
 sudo apt install cuda-toolkit-11-4;
+
+export PATH=/usr/local/cuda-11.4/bin:$PATH;
+export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH;
+
+sudo apt-get update;
+sudo apt-get upgrade;
+
